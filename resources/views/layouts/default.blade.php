f03de4dd90648d1a8742fd910e0ec0bd73a7048d
langhong <949613051@qq.com>
2019-11-28 22:19:16 +0800

    del

--- resources/views/layouts/default.blade.php
+++
@@ -1,8 +0,0 @@
-<htm>
-    <head>
-        <title>@yield('title','Weibo App')  - Laravel 新手入门教程</title>
-    </head>
-    <body>
-    @yield('content')
-    </body>
-</htm>
--- resources/views/static_pages/home.blade.php
+++
@@ -1,4 +0,0 @@
-@extends('layouts.default')
-@section('content')
-    <h1>首页</h1>
-@stop
